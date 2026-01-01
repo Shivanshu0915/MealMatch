d092e0084ecd28e8f9b1779a45f7498a4ebdff94
Piyush Kumar <piyushp6043@gmail.com>
2026-01-01 16:51:47 +0530

    File Handling in food posting configured

--- backend/src/routes/food.route.js
+++ backend/src/routes/food.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import {createFood,getAllFoods,getFoodById,getMyFoods,updateFood,deleteFood} from "../controllers/food.controller.js"
 import { verifyJwt } from "../middlewares/verifyJwt.middleware.js";
+import { upload } from '../middlewares/multer.middleware.js';
 const foodRouter = express.Router();
 foodRouter.get("/", getAllFoods);
-foodRouter.post("/create", verifyJwt, createFood); 
+foodRouter.post("/create", verifyJwt,upload.fields([{ name: "menuImage", maxCount: 1 }]), createFood); 
 foodRouter.get("/me/myposts", verifyJwt, getMyFoods); 
 foodRouter.get("/:id", getFoodById);    
-foodRouter.put("/update/:id", verifyJwt, updateFood); 
+foodRouter.put("/update/:id", verifyJwt,upload.fields([{ name: "menuImage", maxCount: 1 }]), updateFood); 
 foodRouter.delete("/delete/:id", verifyJwt, deleteFood); 
 
 export default foodRouter;
